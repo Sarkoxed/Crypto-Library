5091d67902d5626ef7b5ec1b3041a025c52d427f
Sarkoxed <ss2003@yandex.ru>
2022-02-23 04:02:05 +0300

    done with enc and dec not yet

---
+++ simple_substitution/shift.py
@@ -0,0 +1,66 @@
+from string import ascii_lowercase, ascii_uppercase, punctuation, digits
+from sys import argv
+import json
+
+ext = punctuation + digits
+
+def encrypt(s: str, n: int):
+    s = s.lower()
+    d = dict(zip(ascii_lowercase + ext, ascii_lowercase[1:] + ascii_lowercase[:1] + ext))
+    return "".join(d[x] for x in s)
+
+
+def decrypt(s: str, n: int):
+    s = s.lower()
+    d = dict(zip(ascii_lowercase[1:] + ascii_lowercase[:1] + ext, ascii_lowercase + ext))
+    return "".join(d[x] for x in s)
+
+def brute(s: str):
+    s = s.lower()
+    for i in range(26):
+        a = ascii_lowercase[i:] + ascii_lowercase[:i]
+        d = dict(zip(a + ext, ascii_lowercase + ext))
+        print("".join([d[x] for x in s]))
+
+def freq(s: str):
+    s1 = json.loads(open("frequencies/one.json", "r").read())
+    s2 = json.loads(open("frequencies/bi.json", "r").read())
+    s = s.lower()
+
+    if(len(s) == 0):
+        return
+
+    d1 = dict()
+    for _ in s:
+        d1.setdefault(_, 0)
+        d1[_] += 1
+
+    for i, j in d1.items():
+        d1[i] = j / len(s)
+
+#    ----------------------------------------
+
+
+def main():
+    if(len(argv) == 1):
+        print("--help for help")
+    elif(argv[1] == "help"):
+        print("enc <plain> <rot>")
+        print("dec <cip> <rot>")
+        print("brute <cip>")
+        print("freq <cip>")
+        print("brfreq <cip>")
+    elif(argv[1] == "enc"):
+        encrypt(argv[2], int(argv[3]))
+    elif(argv[1] == "dec"):
+        decrypt(argv[2], int(argv[3]))
+    elif(argv[1] == "brute"):
+        brute(argv[2])
+    elif(argv[1] == "freq"):
+        freq(argv[2])
+    elif(argv[1] == "brfreq"):
+        brfreq(argv[2])
+
+if __name__ == '__main__':
+    main()
+
--- simple_substitution/wiener.py
+++ simple_substitution/wiener.py
@@ -1,3 +1,54 @@
-import owiener
+from string import ascii_lowercase, ascii_uppercase, punctuation, digits
+from sys import argv
+import json
+
+
+ext = punctuation + digits
+
+cip_t = dict()
+for i in range(len(ascii_lowercase)):
+    cip_t[ascii_lowercase[i]] = dict(zip(ascii_lowercase, ascii_lowercase[i:] + ascii_lowercase[:i]))
+
+def encrypt(s: str, k: str):
+    x = len(s) // len(k)
+    k *= (x + 1)
+    return "".join(cip_t[k[i]][s[i]] for i in range(len(s)))
+
+def decrypt(s: str, k: str):
+    x = len(s) // len(k)
+    k *= (x + 1)
+    ans = []
+    for i in range(len(s)):
+        ans.append(list(cip_t['a'].values())[list(cip_t[k[i]].values()).index(s[i])])
+    return "".join(ans)
+
+#def brute(s: str):
+#    #guessing the length of the key
+#    for n in range(1, len(s)):
+
+
+
+def main():
+    if(len(argv) == 1):
+        print("--help for help")
+    elif(argv[1] == "help"):
+        print("enc <plain> <key>")
+        print("dec <cip> <key>")
+        print("brute <cip>")
+        print("freq <cip>")
+        print("brfreq <cip>")
+    elif(argv[1] == "enc"):
+        print(encrypt(argv[2], argv[3]))
+    elif(argv[1] == "dec"):
+        print(decrypt(argv[2], argv[3]))
+#    elif(argv[1] == "brute"):
+#        brute(argv[2])
+#    elif(argv[1] == "freq"):
+#        freq(argv[2])
+#    elif(argv[1] == "brfreq"):
+#        brfreq(argv[2])
+
+if __name__ == '__main__':
+    main()
 
 
