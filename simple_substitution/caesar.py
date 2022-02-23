c50c1c91b59086af61aadecf77d235339309f49e
Sarkoxed <ss2003@yandex.ru>
2022-02-23 04:02:23 +0300

    bye bye

--- simple_substitution/caesar.py
+++
@@ -1,9 +0,0 @@
-from string import ascii_lowercase, ascii_uppercase, punctuation
-s = input("insert a string: ")
-for(i in range(26)):
-    a = ascii_lowercase[i:] + ascii_lowercase[:i]
-    b = ascii_uppercase[i:] + ascii_uppercase[:i]
-    z = dict(zip(a+b+punctuation, ascii_lowercase+ascii_uppercase + punctuation))
-    print("".join([z[x] for x in s]))
-    
-    
