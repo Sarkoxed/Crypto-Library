fa9c2180446f337a999de71036cde19826904a87
Sarkoxed <ss2003@yandex.ru>
2022-06-23 19:00:26 +0300

    Nothing special, just some tests in main

--- elliptic_curves/ecdh.py
+++ elliptic_curves/ecdh.py
@@ -2,20 +2,29 @@ from elliptic_curves import *
 
 
 if __name__ == "__main__":
-    p = int(input("p: "))
-    a1, a2, a3, a4, a6 = [int(input(f"a{i}")) for i in [1,2,3,4,6]]
-    E = EC(a1, a2, a3, a4, a6, GF(p))
-    x, y = int(input("Px")), int(input("Px"))
+    #p = int(input("p: "))
+    #ans = int(input("els count: "))
+    #if(ans == 2):
+    #    a1, a2, a3 = [0]*3
+    #    a4, a6 = int(input("a4: ")), int(input("a6: "))
+    #else:
+    #    a1, a2, a3, a4, a6 = [int(input(f"a{i} ")) for i in [1,2,3,4,6]]
+    #E = EC(a1, a2, a3, a4, a6, GF(p))
+    #x, y = int(input("Px ")), int(input("Px "))
+    p = 1201
+    E = EC(a4=19, a6=17, K=GF(p))
+    x, y = 278, 285
     P = E((x, y, 1))
-
+    print(P)
     na = int(input("Alice's secret key: "))
-    Qa = na * P
+    Qa = P * na
     print(Qa)
 
     m1, m2 = int(input("m1 mod p: ")), int(input("m2 mod p:"))
     k = int(input("k: "))
     R = k * P
     S = k * Qa
+    print(k, S)
     c1 = (S.x() * m1) % p
     c2 = (S.y() * m2) % p
 
