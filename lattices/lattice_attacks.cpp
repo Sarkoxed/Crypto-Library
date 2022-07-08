a104847cceb83d6300bd35c301fda33fd6ed007e
Sarkoxed <ss2003@yandex.ru>
2022-07-09 00:28:54 +0300

    Gaussian Lattice Reduction Hadamard Ratio template

--- lattices/lattice_attacks.cpp
+++ lattices/lattice_attacks.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <math.h>
 
 void Babai_algorithm(Eigen::VectorXf& v, Eigen::VectorXf& w, Eigen::MatrixXf& basis){
     Eigen::VectorXf ans = basis.colPivHouseholderQr().solve(w).array().round().matrix();
     std::cout << ans;
     v = basis * ans;
 }
+
+void Gauss_Lattice_Reduction(Eigen::VectorXd& v1, Eigen::VectorXd& v2){
+    int m;
+    while(true){
+        if(v1.norm() > v2.norm()){
+            std::swap(v1, v2);
+        }
+        auto x = v1.dot(v2);
+        auto r1 = v1.norm();
+        auto r2 = v2.norm();
+        m = round(v1.dot(v2) / (v1.norm() * v1.norm()));
+        if(m == 0){
+            return;
+        }
+        v2 -= m * v1;
+    }
+}
+
+       
--- lattices/lattice_attacks.hpp
+++ lattices/lattice_attacks.hpp
@@ -1,3 +1,19 @@
 #include <Eigen/Dense>
+#include <cmath>
 
 void Babai_algorithm(Eigen::VectorXf&, Eigen::VectorXf&, Eigen::MatrixXf&);
+
+void Gauss_Lattice_Reduction(Eigen::VectorXd&, Eigen::VectorXd&);
+
+template <typename matr>
+float HadamardRatio(matr& b){
+    float de = b.determinant();
+    int d = b.cols();
+    float prod = 1.0;
+    for(int i = 0; i < d; i++){
+        prod *= b.col(i).norm();
+    }
+    return powf(fabs(de) / prod, 1.0 / static_cast<float>(d));
+}
+
+
