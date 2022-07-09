aa434a448c5ace45dc476039bcc0d8346726bb68
Sarkoxed <ss2003@yandex.ru>
2022-07-09 08:41:11 +0300

    Changed round to rint function in Babai algo

--- lattices/lattice_attacks.cpp
+++ lattices/lattice_attacks.cpp
@@ -3,7 +3,7 @@
 #include <math.h>
 
 void Babai_algorithm(Eigen::VectorXf& v, Eigen::VectorXf& w, Eigen::MatrixXf& basis){
-    Eigen::VectorXf ans = basis.colPivHouseholderQr().solve(w).array().round().matrix();
+    Eigen::VectorXf ans = basis.colPivHouseholderQr().solve(w).array().rint().matrix();
     std::cout << ans;
     v = basis * ans;
 }
