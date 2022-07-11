cd5938b0f5a0c24bf54c4d898abf422ffc3c1423
Sarkoxed <ss2003@yandex.ru>
2022-07-11 13:36:01 +0300

    new location

--- lattices/LLL/LLL.hpp
+++
@@ -1,7 +0,0 @@
-#include <Eigen/Dense>
-#include <algorithm>
-
-void LLL(Eigen::MatrixXd&, double delta=0.75, double eta=0.5);
-void reduce(int, int, double, Eigen::MatrixXd&, Eigen::MatrixXd&);
-void swap(int, int, Eigen::MatrixXd&, Eigen::VectorXd&, Eigen::MatrixXd&, Eigen::MatrixXd&);
-bool LLL_check(Eigen::MatrixXd&, double, double);
