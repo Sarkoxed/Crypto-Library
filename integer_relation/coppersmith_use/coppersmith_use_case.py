1c420bcef47044fbd1ee55fb3e1ba65a041ce731
Sarkoxed <ss2003@yandex.ru>
2022-11-28 21:44:11 +0300

    Coppersmith use case

--- integer_relation/coppersmith_use/coppersmith_use_case.py
+++ integer_relation/coppersmith_use/coppersmith_use_case.py
@@ -5,9 +5,15 @@ a = crt(hint, list(range(1, 199)))
 b = lcm(list(range(1, 199)))
 var('x')
 F = Zmod(n)[x]
-x0 = int(F(a + x * b).monic().small_roots(X=2**512 // b, beta=0.4)[0])
+x0 = F(a + x * b).monic().small_roots(X=2**512 // b, beta=0.4)
+print(x0)
+x0 = int(x0[0])
+print(F(a + x * b)(x=x0))
 
-p = a + x0 * b
+p = int(a) + int(x0) * int(b)
+print(p)
+assert p < n
+
 q = n // p
 d = pow(e, -1, (p-1)*(q-1))
 m = pow(c, d, n)
