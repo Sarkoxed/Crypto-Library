ca1a6c5787135adfb4d650dd4e730a20c17e2154
Sarkoxed <ss2003@yandex.ru>
2022-11-30 20:45:05 +0300

    Updated appr_sessions function with user's scaling factor. Done with IV_preceds part

--- rc4/1st_round_attack/1st_round_attack.py
+++ rc4/1st_round_attack/1st_round_attack.py
@@ -4,8 +4,8 @@ from oracle import OracleO as Oracle
 # from oracle import OracleC as Oracle
 
 
-def get_appr_sessions(n: int):
-    return 3 * ceil(17 * n * log(n))
+def get_appr_sessions(n: int, exp: int = 3):
+    return exp * ceil(17 * n * log(n))
 
 
 def recover_kth(key: list[int], n: int, k: int, t: int):
@@ -25,7 +25,7 @@ def attack_iv_follows_key(n: int, keylen: int, guess: int):
     tfreqs = [dict() for _ in range(keylen)]
     ts = [(0, 0) for _ in range(keylen)]
 
-    for _ in range(get_appr_sessions(n)):  # the number of oracle calls
+    for _ in range(get_appr_sessions(n, 3)):  # the number of oracle calls
         o = Oracle(True)
         for i in range(1, keylen):  # retrieving the kth byte of prng
             ti = (i - o.call()) % n
@@ -39,39 +39,80 @@ def attack_iv_follows_key(n: int, keylen: int, guess: int):
         ki = recover_kth(key, n, i, ts[i][0])
         key.append(ki)
 
-    print(key, ts, bytes(key))
+    print(bytes(key))
+    print(o.main_key)
+    return key
 
 
-def attack_iv_preceds_key(n: int, keylen: int):
+def attack_iv_preceds_key_min_storage(n: int, keylen: int):
     o = Oracle(False)
     b = len(o.IV)
 
     key = []
+    for i in range(keylen):
+        freqs, km = dict(), (0, 0)
+        for _ in range(get_appr_sessions(n, 3)):
+            o = Oracle(False)
+            IV = o.IV
 
-    tfreqs = [dict() for _ in range(keylen)]
-    ts = [(0, 0, []) for _ in range(keylen)]
+            for _ in range(1, b + i):
+                o.call()
 
-    for _ in range(get_appr_sessions(n)):
+            ti = (b + i - o.call()) % n
+            ki = recover_kth(list(IV) + key, n, b + i, ti)
+
+            freqs.setdefault(ki, 0)
+            freqs[ki] += 1
+
+            if freqs[ki] > km[1]:
+                km = (ki, freqs[ki])
+
+        key.append(km[0])
+        print(bytes(key))
+
+    print(bytes(key))
+    print(o.main_key, o.IV)
+    return bytes(key)
+
+
+def attack_iv_preceds_key_min_time(n: int, keylen: int):
+    o = Oracle(False)
+    b = len(o.IV)
+
+    key = []
+    keystreams = []
+
+    print("started oracle calls")
+    for _ in range(get_appr_sessions(n, 2)):
         o = Oracle(False)
         IV = o.IV
-
-        for i in range(1, b):
+        for _ in range(1, b):
             o.call()
 
+        ts = []
         for i in range(keylen):
             ti = (b + i - o.call()) % n
-            tfreqs[i].setdefault(ti, 0)
-            tfreqs[i][ti] += 1
+            ts.append(ti)
 
-            if tfreqs[i][ti] > ts[i][1]:
-                ts[i] = (ti, tfreqs[i][ti], list(IV))
+        keystreams.append((ts, IV))
 
+    print("started analyzing data")
     for i in range(keylen):
-        ki = recover_kth(ts[i][2] + key, n, b + i, ts[i][0])
-        key.append(ki)
+        freqsi, km = dict(), (0, 0)
+        for ts, IV in keystreams:
+            ki = recover_kth(list(IV) + key, n, b + i, ts[i])
 
-    print(key, ts[:10], bytes(key), len(key))
+            freqsi.setdefault(ki, 0)
+            freqsi[ki] += 1
+
+            if freqsi[ki] > km[1]:
+                km = (ki, freqsi[ki])
+        key.append(km[0])
+
+    print(bytes(key))
+    print(o.main_key)
+    return bytes(key)
 
 
-#attack_iv_follows_key(n=256, keylen=28, guess=ord('O'))
-attack_iv_preceds_key(n=256, keylen=14)
+# attack_iv_follows_key(n=256, keylen=28, guess=ord('O'))
+attack_iv_preceds_key_min_time(n=256, keylen=31)
