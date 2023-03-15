b604f0761da6170cb10ad182374ae082f3035e2c
Sarkoxed <sarkoxed2013@yandex.ru>
2023-03-15 04:47:17 +0300

    science bitch

--- tools/inverse/inverse.py
+++ tools/inverse/inverse.py
@@ -9,10 +9,47 @@ def get_pair(a, b):
         z2 = (z2[1], z2[0] - d * z2[1])
     return (r, z1, z2)
 
+def prod_m(l, p):
+    ans = 1
+    for x in l:
+        ans = (ans * x) % p
+    return ans
 
-from pprint import pprint
+def montgomery(l, p):
+    a = prod_m(l, p)
+    a_i = pow(a, -1, p)
+    res = [(prod_m(l[:i] + l[i + 1:], p) * a_i) % p for i in range(len(l))]
+    return res
 
-a = int(input())
-b = int(input())
+if __name__ == "__main__":
+    #from pprint import pprint
+    #
+    #a = int(input())
+    #b = int(input())
+    #
+    #pprint(get_pair(a, b))
 
-pprint(get_pair(a, b))
+    from time import time
+    from Crypto.Util.number import getPrime
+    from random import randint
+    
+    p = getPrime(1024)
+    n = 1000
+    l = [randint(0, p-1) for _ in range(n)]
+    
+    start = time()
+    m = [pow(x, p-2, p) for x in l]
+    end = time()
+    print(f"pow(x, p-2, p): {end - start}")
+    
+    start = time()
+    m1 = [pow(x, -1, p) for x in l]
+    end = time()
+    print(f"pow(x, -1, p): {end - start}")
+ 
+    start = time()
+    m2 = montgomery(l, p)
+    end = time()
+    print(f"mont: {end - start}")
+
+    assert m1 == m2 and m1 == m
