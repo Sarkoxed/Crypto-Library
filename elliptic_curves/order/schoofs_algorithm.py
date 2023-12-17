2c628443d0e235aceabd3c86ed087417b3071456
Sarkoxed <sarkoxed2013@yandex.ru>
2023-12-17 05:25:59 +0300

    blacked and sorted

--- elliptic_curves/order/schoofs_algorithm.py
+++ elliptic_curves/order/schoofs_algorithm.py
@@ -1,8 +1,15 @@
 # https://www.mat.uniroma2.it/~schoof/ctpts.pdf
 
-from division_polynomial import init_cache_noy, psi_cached_noy, phi_cached_noy, omega_cached_noy
-from sage.all import var, GF, PolynomialRing, Primes, gcd, randint, prod, crt
 from Crypto.Util.number import getPrime
+from sage.all import GF, PolynomialRing, Primes, crt, gcd, prod, randint, var
+
+from division_polynomial import (
+    init_cache_noy,
+    omega_cached_noy,
+    phi_cached_noy,
+    psi_cached_noy,
+)
+
 
 def get_base(q):
     base = []
@@ -14,37 +21,19 @@ def get_base(q):
             break
     return base
 
-#def get_x_j_psi(x, y_squared, cache, j): # TODO handle y^2
-#    psin_1 = psi_cached_noy(abs(j) - 1, cache, y_squared)
-#    psin1  = psi_cached_noy(abs(j) + 1, cache, y_squared)
-#    psin   = psi_cached_noy(abs(j), cache, y_squared)
-#
-#    x_j_num = x * psin**2 - psin_1 * psin1
-#    x_j_den = psin**2
-#    return x_j_num, x_j_den
-#
-#def get_y_j_div_y_psi(x, y_squared, cache, j): # works
-#    psin   = psi_cached_noy(abs(j), cache, y_squared)
-#    psin2n = psi_cached_noy(2 * abs(j), cache, y_squared)
-#
-#    y_j_num = psi2n
-#    y_j_den = 2 * psin**4
-#    if j < 0:
-#        y_j_num = -y_j_num
-#    return y_j_num, y_j_den
-
 
 def get_x_j(x, y_squared, cache, j):
     x_j_num = phi_cached_noy(abs(j), cache, x, y_squared)
-    x_j_den = psi_cached_noy(abs(j), cache, y_squared)**2
+    x_j_den = psi_cached_noy(abs(j), cache, y_squared) ** 2
     if abs(j) % 2 == 0:
         x_j_den *= y_squared
-    
+
     return x_j_num, x_j_den
+
 
 def get_y_j_div_y(x, y_squared, cache, j):
     y_j_num = omega_cached_noy(abs(j), cache, y_squared)
-    y_j_den = psi_cached_noy(abs(j), cache, y_squared)**3
+    y_j_den = psi_cached_noy(abs(j), cache, y_squared) ** 3
     if j < 0:
         y_j_num = -y_j_num
 
@@ -52,6 +41,7 @@ def get_y_j_div_y(x, y_squared, cache, j):
         y_j_den *= y_squared**2
 
     return y_j_num, y_j_den
+
 
 # (x1, y1) = (x^(p^2), y^(p^2)) + pl * (x, y)
 # y^2 = x^3 + A * x + B
@@ -66,32 +56,41 @@ def get_y_j_div_y(x, y_squared, cache, j):
 # odd case equations
 def get_x1(x, y_squared, cache, q, ql):
     x_ql_num, x_ql_den = get_x_j(x, y_squared, cache, ql)
-    
-    if not (x_ql_num - x_ql_den * x**(q**2)).is_unit():
-        return None, (x_ql_num - x_ql_den * x**(q**2))
+
+    if not (x_ql_num - x_ql_den * x ** (q**2)).is_unit():
+        return None, (x_ql_num - x_ql_den * x ** (q**2))
 
     y_ql_num, y_ql_den = get_y_j_div_y(x, y_squared, cache, ql)
 
-    x_q2 = x**(q**2)
-    lambda_2_num = y_squared * (y_squared**((q**2 - 1)//2) * y_ql_den - y_ql_num)**2 * x_ql_den**2
-    lambda_2_den = y_ql_den**2 * (x_q2 * x_ql_den - x_ql_num)**2
-    
-    x1_num = lambda_2_num * x_ql_den - x_ql_num * lambda_2_den - x_q2 * lambda_2_den * x_ql_den
+    x_q2 = x ** (q**2)
+    lambda_2_num = (
+        y_squared
+        * (y_squared ** ((q**2 - 1) // 2) * y_ql_den - y_ql_num) ** 2
+        * x_ql_den**2
+    )
+    lambda_2_den = y_ql_den**2 * (x_q2 * x_ql_den - x_ql_num) ** 2
+
+    x1_num = (
+        lambda_2_num * x_ql_den
+        - x_ql_num * lambda_2_den
+        - x_q2 * lambda_2_den * x_ql_den
+    )
     x1_den = lambda_2_den * x_ql_den
 
     return x1_num, x1_den
 
+
 # y1 = lambda * (x_pl - x1) - y_pl
 #
 # y1 = (y^(p^2) - omega(x) / psi(x)^3 * y) / (x^(p^2) - phi(x)/psi(x)^2) * (phi(x) / psi(x)^2 - x1_num / x1_den) - omega(x)/psi(x)^3 * y
-# y1 / y = ((y^2)^((p^2-1)//2) * psi(x)^3 - omega(x)) / (x^(p^2) * psi(x)^3 - phi(x) * psi(x)) *  (phi(x) * x1_den - x1_num * psi(x)^2) / (psi(x)^2 * x1_den) - omega(x) / psi(x)^3  
+# y1 / y = ((y^2)^((p^2-1)//2) * psi(x)^3 - omega(x)) / (x^(p^2) * psi(x)^3 - phi(x) * psi(x)) *  (phi(x) * x1_den - x1_num * psi(x)^2) / (psi(x)^2 * x1_den) - omega(x) / psi(x)^3
 # for n is odd. in even case consider dividing x by y^2 and y by y^4
 def get_y1_div_y(x, y_squared, x1_num, x1_den, cache, q, ql):
     x_ql_num, x_ql_den = get_x_j(x, y_squared, cache, ql)
     y_ql_num, y_ql_den = get_y_j_div_y(x, y_squared, cache, ql)
 
-    lambda_num = (y_squared ** ((q**2 - 1)//2) * y_ql_den - y_ql_num) * x_ql_den
-    lambda_den = (x**(q**2) * x_ql_den - x_ql_num) * y_ql_den
+    lambda_num = (y_squared ** ((q**2 - 1) // 2) * y_ql_den - y_ql_num) * x_ql_den
+    lambda_den = (x ** (q**2) * x_ql_den - x_ql_num) * y_ql_den
 
     left_num = lambda_num * (x_ql_num * x1_den - x1_num * x_ql_den)
     left_den = lambda_den * (x_ql_den * x1_den)
@@ -101,74 +100,79 @@ def get_y1_div_y(x, y_squared, x1_num, x1_den, cache, q, ql):
 
     return y1_num, y1_den
 
+
 def trace_mod_2(A, B, P, x):
     q = P.base_ring().order()
     mod = P(x**3 + A * x + B)
     Q = P.quotient(mod)
-    left = Q(x)**q - Q(x)
+    left = Q(x) ** q - Q(x)
     if left.is_unit():
         return 1
     return 0
+
 
 def trace_mod_l(A, B, P, x, y_squared, l, cache_l):
     q = P.base_ring().order()
     ql = q % l
     if 2 * ql > l:
         ql = ql - l
-    
+
     psi_l = psi_cached_noy(l, cache_l, y_squared)
     Q = P.quotient_ring(psi_l)
     cache = init_cache_noy(A, B, Q(x))
-    
+
     x1_num, x1_den = get_x1(Q(x), Q(y_squared), cache, q, ql)
-    
+
     if x1_num is not None:
-        for j in range(1, (l-1)//2 + 1):
+        for j in range(1, (l - 1) // 2 + 1):
             x_j_num, x_j_den = get_x_j(Q(x), Q(y_squared), cache, j)
             x_q_j_num = x_j_num**q
             x_q_j_den = x_j_den**q
-        
-            x1_x_j = x1_num * x_q_j_den - x_q_j_num * x1_den 
-            # x1 - x^q_j == 0? (x1, y1) == j (x^p, +-y^p) ? 
+
+            x1_x_j = x1_num * x_q_j_den - x_q_j_num * x1_den
+            # x1 - x^q_j == 0? (x1, y1) == j (x^p, +-y^p) ?
             if x1_x_j == 0:
                 y_j_num, y_j_den = get_y_j_div_y(Q(x), Q(y_squared), cache, j)
                 # need extra argument for (r2(x) * y)^p cache, j)
-                y_q_j_num = y_j_num**q * Q(y_squared)**((q - 1)//2) 
+                y_q_j_num = y_j_num**q * Q(y_squared) ** ((q - 1) // 2)
                 y_q_j_den = y_j_den**q
-        
-                y1_num, y1_den = get_y1_div_y(Q(x), Q(y_squared), x1_num, x1_den, cache, q, ql)
-        
+
+                y1_num, y1_den = get_y1_div_y(
+                    Q(x), Q(y_squared), x1_num, x1_den, cache, q, ql
+                )
+
                 y1_y_j_div_y = y1_num * y_q_j_den - y1_den * y_q_j_num
                 if y1_y_j_div_y == 0:
                     return j
                 else:
-                    return l-j
+                    return l - j
         else:
             raise ValueError
 
     ql = GF(l)(q)
     if not ql.is_square():
         return 0
-    
+
     w = ql.sqrt()
     x_w_num, x_w_den = get_x_j(Q(x), Q(y_squared), cache, int(w))
-    
-    x_q_x_w = Q(x)**q * x_w_den - x_w_num
+
+    x_q_x_w = Q(x) ** q * x_w_den - x_w_num
     if x_q_x_w.is_unit():
         return 0
-    
+
     y_w_num, y_w_den = get_y_j_div_y(Q(x), Q(y_squared), cache, int(w))
-    y_q_div_y = Q(y_squared)**((q - 1)//2)
-        
+    y_q_div_y = Q(y_squared) ** ((q - 1) // 2)
+
     y_q_y_w_div_y = y_q_div_y * y_w_den - y_w_num
     if y_q_y_w_div_y.is_unit():
         return int(-2 * w)
     return int(2 * w)
 
+
 def get_order(A, B, q):
     base = get_base(q)
-    
-    x = var('x')
+
+    x = var("x")
     P = PolynomialRing(GF(q), x)
     y_squared = P(x**3 + A * x + B)
     al = []
@@ -179,12 +183,9 @@ def get_order(A, B, q):
             al.append(trace_mod_2(A, B, P, x))
         else:
             al.append(trace_mod_l(A, B, P, P(x), y_squared, l, cache_l))
-    #print(f"{base=}")
-    #print(f"{al =}")       
     a = int(crt(al, base))
     mod = prod(base)
-    
+
     while a**2 > 4 * q:
         a -= mod
-    #print(a)
     return q + 1 - a
--- elliptic_curves/order/schoofs_algorithm_test.py
+++ elliptic_curves/order/schoofs_algorithm_test.py
@@ -1,7 +1,9 @@
 from schoofs_algorithm import *
 
+
 def test_get_xy(n=1):
-    from sage.all import random_prime, GF, EllipticCurve
+    from sage.all import GF, EllipticCurve, random_prime
+
     for _ in range(n):
         p = random_prime(2**16)
         a = randint(0, p)
@@ -16,47 +18,51 @@ def test_get_xy(n=1):
         t1, t2 = get_x1(gx, gx**3 + a * gx + b, cache, p**2, pl)
         w1, w2 = get_y1_div_y(gx, gx**3 + a * gx + b, t1, t2, cache, p, pl)
         x11, y11 = (t1 / t2, w1 / w2 * gy)
-        
+
         assert e((x11, y11)) == r1
 
+
 def test_j_exists():
-    from sage.all import random_prime, GF, EllipticCurve
+    from sage.all import GF, EllipticCurve, random_prime
+
     a, b, p = 2, 1, 19
     l = 5
-    x = var('x')
+    x = var("x")
     P = PolynomialRing(GF(p), x)
     cache = init_cache_noy(a, b, P(x))
     psi_l = psi_cached_noy(l, cache, P(x**3 + a * x + b))
-    
+
     e = EllipticCurve(GF(p**12), [a, b])
     g = e.gens()[1] * (e.order() // 25)
     assert g.order() == 5
     gx, gy = g.xy()
     assert psi_l(x=gx) == 0
-    
+
     Q = P.quotient(psi_l)
     cache1 = init_cache_noy(a, b, Q(x))
     x1_num, x1_den = get_x1(Q(x), Q(x**3 + a * x + b), cache1, p, -1)
     x_j_num, x_j_den = get_x_j(Q(x), Q(x**3 + a * x + b), cache1, 2)
     x_q_j_num = x_j_num**p
     x_q_j_den = x_j_den**p
-    
+
     res_num = x1_num * x_q_j_den - x1_den * x_q_j_num
     assert res_num == 0
 
-    y1_num, y1_den = get_y1_div_y(Q(x), Q(x**3 + a * x + b), x1_num, x1_den, cache1, p, -1)
+    y1_num, y1_den = get_y1_div_y(
+        Q(x), Q(x**3 + a * x + b), x1_num, x1_den, cache1, p, -1
+    )
     y_j_num, y_j_den = get_y_j_div_y(Q(x), Q(x**3 + a * x + b), cache1, 2)
-    y_q_j_num = y_j_num**p * Q(x**3 + a * x + b)**((p-1)//2)
+    y_q_j_num = y_j_num**p * Q(x**3 + a * x + b) ** ((p - 1) // 2)
     y_q_j_den = y_j_den**p
-    
-    #res_num = y1_num * y_q_j_den - y1_den * y_q_j_num
+
+    # res_num = y1_num * y_q_j_den - y1_den * y_q_j_num
     res_num = y1_num * y_q_j_den + y1_den * y_q_j_num
     assert res_num == 0
 
 
-def test(n = 1, nbit = 50):
-    from sage.all import EllipticCurve, randint
+def test(n=1, nbit=50):
     from Crypto.Util.number import getPrime
+    from sage.all import EllipticCurve, randint
 
     for _ in range(n):
         p = getPrime(nbit)
@@ -64,37 +70,21 @@ def test(n = 1, nbit = 50):
         b = randint(0, p)
         e = EllipticCurve(GF(p), [a, b])
         print(e)
-        
+
         assert get_order(a, b, p) == e.order()
+
 
 def test_textbook():
     from sage.all import EllipticCurve
+
     p = 19
-    a = 2 
+    a = 2
     b = 1
     print(get_order(a, b, p))
 
-def errors():
-    from sage.all import EllipticCurve, randint
-    a = 678415
-    b = 756009
-    p = 859633
-    e = EllipticCurve(GF(p), [a, b])
-    
-    t = e.trace_of_frobenius()
-    print(f"{t=}")
-    base = get_base(p)
-    print("order: ", e.order())
-    print()
-    print("remainders: ", [t % l for l in base])
-    print()
- 
-    print(get_order(a, b, p))
-    print(e.order())
 
 if __name__ == "__main__":
     test_get_xy(10)
     test_j_exists()
     test_textbook()
     test(10, 60)
-#    errors()
