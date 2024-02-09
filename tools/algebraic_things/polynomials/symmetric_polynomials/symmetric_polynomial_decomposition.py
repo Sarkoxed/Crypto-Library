549790e16349e843cd0aa9c39d5c434e756f2c64
Sarkoxed <sarkoxed2013@yandex.ru>
2024-02-09 15:10:52 +0300

    Blacked

--- tools/algebraic_things/polynomials/symmetric_polynomials/symmetric_polynomial_decomposition.py
+++ tools/algebraic_things/polynomials/symmetric_polynomials/symmetric_polynomial_decomposition.py
@@ -1,8 +1,9 @@
-from sage.all import * 
+from sage.all import QQ, PolynomialRing, expand
 from itertools import permutations
 
 # everything could be done using SymmetricFunctions but whatever
 # Thanks https://www.youtube.com/watch?v=50pXtgintLc&ab_channel=diplomaticfish
+
 
 def get_poly(gens, powers):
     res_poly = 0
@@ -21,51 +22,58 @@ def get_basis(gens):
     basis_polys = []
     for k in range(1, n + 1):
         monomial = [1 for _ in range(k)] + [0 for _ in range(n - k)]
-        poly_powers = list(set(permutations(monomial))) # avoiding duplicates
+        poly_powers = list(set(permutations(monomial)))  # avoiding duplicates
         basis_polys.append(get_poly(gens, poly_powers))
     return basis_polys
+
 
 def decompose(f, basis):
     n = len(basis)
     f = expand(f)
     assert f.is_symmetric()
-    
-    sym_basis = list(PolynomialRing(QQ, n, "s").gens())
 
+    sym_basis = list(PolynomialRing(QQ, n, "s").gens())
 
     dec = 0
     while not f.is_constant():
         tmp = [(c[0], c[1].degrees()) for c in list(f)]
         cf, degree = max(tmp, key=lambda x: x[1])
-        
-        assert tuple(sorted(degree, reverse=True)) == degree # sym check
+
+        assert tuple(sorted(degree, reverse=True)) == degree  # sym check
 
         tmpoly = 1
         tmpsym = 1
         for k in range(1, n):
-            curpower = degree[k-1] - degree[k]
-            tmpoly *= basis[k-1]**curpower
-            tmpsym *= sym_basis[k-1]**curpower
+            curpower = degree[k - 1] - degree[k]
+            tmpoly *= basis[k - 1] ** curpower
+            tmpsym *= sym_basis[k - 1] ** curpower
 
-        tmpoly *= basis[-1]**degree[-1]
+        tmpoly *= basis[-1] ** degree[-1]
         tmpsym *= sym_basis[-1] ** degree[-1]
 
         tmpoly *= cf
         tmpsym *= cf
 
-        f -= tmpoly;
+        f -= tmpoly
         dec += tmpsym
     dec += int(f)
     return dec
 
-P = PolynomialRing(QQ, 3, "x")
-gens = list(P.gens())
 
-basis = get_basis(gens)
+def test():
+    P = PolynomialRing(QQ, 3, "x")
+    gens = list(P.gens())
+    print(gens)
 
-x0, x1, x2 = gens 
-for n in range(1, 5):
-    f = P(x0**n + x1**n + x2**n)
-    
-    dec = decompose(f, basis)
-    print(dec)
+    basis = get_basis(gens)
+
+    x0, x1, x2 = gens
+    for n in range(1, 5):
+        f = P(x0**n + x1**n + x2**n)
+
+        dec = decompose(f, basis)
+        print(dec)
+
+
+if __name__ == "__main__":
+    test()
